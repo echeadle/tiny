977bc965cd9647dc8d7a4cc81425c84c59e55abc
Edward Cheadle <edward.cheadle8@gmail.com>
2021-02-06 17:07:44 -0700

    Ran pylint and fixed all issues

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""
+# Original Author: Ken Youens-Clark <kyclark@gmail.com>
 # Purpose: Say hello
-
+"""
 import argparse
 
-
 def get_args():
+    """Get command-line arguments"""
+
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('-n', '--name', metavar='name',
                         default='World', help='Name to greet')
@@ -12,6 +15,8 @@ def get_args():
 
 
 def main():
+    """Program Entry Point"""
+
     args = get_args()
     print('Hello, ' + args.name + '!')
 
