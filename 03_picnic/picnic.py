e7d29a8471cd75282768a87bf3e9dd3e7e4e2ff2
Edward Cheadle <edward.cheadle8@gmail.com>
2021-02-07 16:30:56 -0700

    Fixed arg parser and printing out arguments.

--- 03_picnic/picnic.py
+++ 03_picnic/picnic.py
@@ -2,71 +2,45 @@
 """
 Author : echeadle <echeadle@localhost>
 Date   : 2021-02-07
-Purpose: Create a Picnic Food List
+Purpose: Picnic Game
 """
 
 import argparse
-
 
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Picnic Game',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Picnic Game",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
+    parser.add_argument("item",
+                        metavar="str",
+                        nargs= '+',
+                        help="Item(s) to bring")
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument("-s",
+                        "--sorted",
+                        help="Sort the items",
+                        action="store_true")
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Picnic Program Entry Point"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    flag_arg = args.sorted
+    pos_arg = args.item
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print(f'items = "{pos_arg}"')
+    print(f'Is list sorted? {flag_arg}')
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
