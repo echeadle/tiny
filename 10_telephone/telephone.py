cf9401ab0e165bba08b42c4cd6fc86a95abe6933
Edward Cheadle <edward.cheadle8@gmail.com>
2021-02-15 13:43:22 -0700

    Finisehed chps 10 and 11

--- 10_telephone/telephone.py
+++ 10_telephone/telephone.py
@@ -44,7 +44,7 @@ def get_args():
         parser.error(f'--mutations "{args.mutations}" must be between 0 and 1')
 
     if os.path.isfile(args.text):
-        args.text = open(args.text.read().rstrip())
+        args.text = open(args.text).read().rstrip()
 
     return args
 
@@ -54,6 +54,18 @@ def main():
     """ Main program entry point """
 
     args = get_args()
+    text = args.text
+    random.seed(args.seed)
+    alpha = "".join(sorted(string.ascii_letters + string.punctuation))
+    len_text = len(text)
+    num_mutations = round(args.mutations * len_text)
+    new_text = text
+
+    for i in random.sample(range(len_text), num_mutations):
+        new_char = random.choice(alpha.replace(new_text[i], ""))
+        new_text = new_text[:i] + new_char + new_text[i + 1 :]
+
+    print(f'You said: "{text}"\nI heard : "{new_text}"')
 
 
 # --------------------------------------------------
